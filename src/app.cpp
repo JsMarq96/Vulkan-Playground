b316d5e4e035345ade0f4bc3e39131eccfc6c762
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2023-02-03 16:46:02 +0100

    Finished set up, now to swapchain & window context

---
+++ src/app.cpp
@@ -0,0 +1,261 @@
+#include "app.h"
+#include <cstdint>
+#include <cstring>
+#include <cstdint>
+#include <optional>
+#include <stdint.h>
+
+bool check_validation_layers(const char** required_val_layers, 
+                             const uint32_t required_val_layer_count);
+
+bool is_device_suitable(const VkPhysicalDevice &device, sQueueFamilies* queues);
+
+static VKAPI_ATTR VkBool32 
+VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                          VkDebugUtilsMessageTypeFlagsEXT message_type,
+                          const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+                          void* user_data);
+
+
+void sApp::_init_vulkan() {
+    // ===================================
+    // LOAD EXTENSIONLIST ================
+    // ===================================
+    {
+        // Get the required GLFW extensions & add it to the list of the required exts.
+        uint32_t glfw_extension_count = 0;
+        const char** glfw_extensions;
+        glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+        for(uint16_t i = 0; i < glfw_extension_count; i++) {
+            Vulkan.required_extensions[Vulkan.required_extension_count++] = glfw_extensions[i];
+        }
+
+        std::cout  << "Enabled extensions: " << std::endl;
+        for(uint16_t i = 0; i < Vulkan.required_extension_count; i++) {
+            std::cout  << " - " << (const char*)Vulkan.required_extensions[i] << std::endl;
+        }
+    }
+
+    // ===================================
+    // CREATE VULKAN INSTANCE ============
+    // ===================================
+    {
+         VkApplicationInfo app_info = {
+            .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .pNext = NULL,
+            .pApplicationName = WINDOW_NAME,
+            .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = ENGINE_NAME,
+            .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = VK_API_VERSION_1_0
+        };
+
+        VkInstanceCreateInfo create_info = {
+            .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pNext = NULL,
+            .pApplicationInfo = &app_info,
+            .enabledLayerCount = 0,
+            .enabledExtensionCount = Vulkan.required_extension_count,
+            .ppEnabledExtensionNames = (const char**) Vulkan.required_extensions,
+        };
+
+        // Enable validation layers on debug 
+#ifndef NDEBUG
+        assert_msg(check_validation_layers(Vulkan.required_validation_layers, 
+                                           Vulkan.required_validation_layer_count), 
+                   "Validation layers not found");
+        create_info.enabledLayerCount = Vulkan.required_validation_layer_count;
+        create_info.ppEnabledLayerNames = Vulkan.required_validation_layers;
+
+        VkDebugUtilsMessengerCreateInfoEXT debug_utils_create_info = {
+            .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+            .pNext = NULL,
+            .messageSeverity =  VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+            .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+            .pfnUserCallback = debug_callback,
+            .pUserData = NULL // Not needed
+        };
+
+        // Why??
+        create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debug_utils_create_info;
+#endif
+
+        VK_OK(vkCreateInstance(&create_info, NULL, &Vulkan.instance), "Create instance");
+    }
+
+    // ===================================
+    // ENABLE DEBUG MESSENGE =============
+    // ===================================
+#ifndef NDEBUG
+    {
+        auto vkCreateDebugUtilsMessengerEXT = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(Vulkan.instance, 
+                                                                                                         "vkCreateDebugUtilsMessengerEXT");
+        assert_msg(vkCreateDebugUtilsMessengerEXT != NULL,
+                  "Could not load the extension for debug messenger");
+
+        VkDebugUtilsMessengerCreateInfoEXT create_info = {
+            .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+            .pNext = NULL,
+            .messageSeverity =  VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+            .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+            .pfnUserCallback = debug_callback,
+            .pUserData = NULL // Not needed
+        };
+
+        vkCreateDebugUtilsMessengerEXT(Vulkan.instance, 
+                                       &create_info, 
+                                       NULL, 
+                                       &Vulkan.debug_messenger);
+    }
+#endif
+
+
+    // ===================================
+    // PICK PHYSICAL DEVICE ==============
+    // ===================================
+    {
+        uint32_t device_count = 0;
+        vkEnumeratePhysicalDevices(Vulkan.instance, 
+                                   &device_count, 
+                                   NULL);
+
+        assert_msg(device_count > 0, "There are no GPUs detected!");
+
+        VkPhysicalDevice *device_list = (VkPhysicalDevice*) malloc(sizeof(VkPhysicalDevice) * device_count);
+        vkEnumeratePhysicalDevices(Vulkan.instance, 
+                                   &device_count, 
+                                   device_list);
+        
+        // TODO: create a reting system for each GPU's capabilities
+        for(uint32_t i = 0; i < device_count; i++) {
+            if (is_device_suitable(device_list[i], 
+                                   &Vulkan.queues)) {
+                Vulkan.physical_device = device_list[i];
+                break;
+            }
+        }
+
+        assert_msg(Vulkan.physical_device != VK_NULL_HANDLE, "Could not find a suitable GPU");
+
+        free(device_list);
+    }
+
+
+    // ===================================
+    // CREATE LOGICAL DEVICE =============
+    // ===================================
+    {
+        // Set the queues for the device
+        float queue_priority = 1.0f;
+        VkDeviceQueueCreateInfo queue_create_info = {
+            .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+            .pNext = NULL,
+            .queueFamilyIndex = Vulkan.queues.graphics_family_id,
+            .queueCount = 1,
+            .pQueuePriorities = &queue_priority
+        };
+
+        // Set the device features: no need for now (thingslike geometry shaders and stuff)
+        VkPhysicalDeviceFeatures device_features{};
+
+        // TODO: add the enabled layers for retorcompatibility
+        VkDeviceCreateInfo device_create_info = {
+            .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+            .pNext = NULL,
+            .queueCreateInfoCount = 1,
+            .pQueueCreateInfos = &queue_create_info,
+            .enabledExtensionCount = 0,
+            .pEnabledFeatures = &device_features,
+        };
+
+        VK_OK(vkCreateDevice(Vulkan.physical_device, 
+                             &device_create_info, 
+                             NULL, 
+                             &Vulkan.device),
+             "Create a logical device");
+        
+        // Get the queues of the newly created device
+        vkGetDeviceQueue(Vulkan.device, 
+                         Vulkan.queues.graphics_family_id, 
+                         0, 
+                         &Vulkan.graphics_queue);
+    }
+}
+
+
+
+// ===================================
+// HELPER FUNCTIONS
+// ===================================
+bool check_validation_layers(const char** required_val_layers, 
+                             const uint32_t required_val_layer_count) {
+    // Get the layer count
+    uint32_t layer_count = 0;
+    vkEnumerateInstanceLayerProperties(&layer_count, NULL);
+
+    VkLayerProperties* available_layers = (VkLayerProperties*) malloc(sizeof(VkLayerProperties) * layer_count);
+    vkEnumerateInstanceLayerProperties(&layer_count, available_layers);
+
+    for(uint32_t i = 0; i < required_val_layer_count; i++) {
+        bool layer_found = false;
+
+        for(uint32_t j = 0; j < layer_count; j++) {
+            if (strcmp(required_val_layers[i], available_layers[j].layerName) == 0) {
+                layer_found = true;
+                break;
+            }
+        }
+
+        if (!layer_found) {
+            return false;
+        }
+    }
+    
+    free(available_layers);
+
+    return true;
+}
+
+
+bool is_device_suitable(const VkPhysicalDevice &device, sQueueFamilies* queues) {
+    // TODO: check for VkPhyscallDeviceFeatures, for things like texture compression, multiviewport etc
+    VkPhysicalDeviceProperties device_properties = {};
+    vkGetPhysicalDeviceProperties(device, 
+                                  &device_properties);
+
+
+    if (device_properties.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+        return false;
+    }
+
+    // Now check the the needed queue families
+    uint32_t queue_family_count = 0;
+
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, NULL);
+
+    VkQueueFamilyProperties *queue_properties = (VkQueueFamilyProperties*) malloc(sizeof(VkQueueFamilyProperties) * queue_family_count); 
+
+    for(uint32_t i = 0; i < queue_family_count; i++) {
+        if (queue_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            queues->graphics_family_id = i;
+            queues->has_found_graphics_family = true;
+        }
+    }
+
+    free(queue_properties);
+
+    return queues->has_found_graphics_family = true;
+}
+
+
+static VKAPI_ATTR VkBool32 
+VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                          VkDebugUtilsMessageTypeFlagsEXT message_type,
+                          const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+                          void* user_data) {
+    if (message_severity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+        std::cerr << "Validation layer error: " << callback_data->pMessage << std::endl;
+    }
+    return VK_FALSE;
+}
--- src/app.h
+++ src/app.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -9,12 +10,44 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define WINDOW_NAME   "Vulkan test"
+#define ENGINE_NAME   "No engine"
 
 #define assert_msg(condition, msg) if (!(condition)) {std::cout << msg << std::endl; assert(false);}
+#define VK_OK(result, msg) if ((result) != VK_SUCCESS) { std::cout << "Vulkan validation error: " << result << " on " << msg << std::endl; assert(false);}
 
+struct sQueueFamilies {
+    uint32_t graphics_family_id;
+    bool has_found_graphics_family = false;
+};
+
+// https://vulkan-tutorial.com/en/Drawing_a_triangle/Presentation/Window_surface
 struct sApp {
     GLFWwindow *window = NULL;
 
+    // Vulkan data
+    struct {
+        VkInstance instance;
+        VkPhysicalDevice physical_device = VK_NULL_HANDLE;
+        sQueueFamilies queues;
+        VkDevice device; // logical device
+        VkQueue  graphics_queue;
+
+#ifndef NDEBUG
+        // Validation layers
+        const char* required_validation_layers[2] = {
+            "VK_LAYER_KHRONOS_validation",
+        };
+        uint32_t required_validation_layer_count = 1;
+
+        const char* required_extensions[10] = {
+            VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+        };
+        uint32_t required_extension_count = 1;
+
+        // Debug messaegs
+        VkDebugUtilsMessengerEXT debug_messenger;
+#endif
+    } Vulkan;
 
     void run() {
         _init_window();
@@ -50,16 +83,17 @@ struct sApp {
                               int mods) {
                                 if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
                                     glfwSetWindowShouldClose(window, 
-                                                            GL_TRUE);
+                                                             GL_TRUE);
                                 }
                             });
     }
 
-    void _init_vulkan() {
-        // TODO
-    }
+    void _init_vulkan();
 
     void _clean_up() {
+        vkDestroyDevice(Vulkan.device, NULL);
+        // TODO destroy the Utils messener: add it to the Vulkna struct
+        vkDestroyInstance(Vulkan.instance, NULL);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
